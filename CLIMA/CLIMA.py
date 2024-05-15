3bf7baefe7f1cfb83639a2b30c01ff2735a860f5
Luís Nunes <lnluisnunes2005@gmail.com>
2024-05-15 03:51:28 +0100

    Update CLIMA.py

--- CLIMA/CLIMA.py
+++ CLIMA/CLIMA.py
@@ -3,28 +3,10 @@ import tkinter as tk
 import requests_cache
 import pandas as pd
 from retry_requests import retry
-"""import yagmail
+import datetime
+from geopy.geocoders import Nominatim
 
-#Função para enviar email a user
-def enviar_email():
-     
-    # Configurar yagmail com sua conta do Gmail
-    remetente = 'trabalho.lab.clima@gmail.com'
-    senha = 'VamosTirar20.'
-    destinatario = 'lnluisnunes2005@gmail.com'
-    assunto = 'Assunto do E-mail'
-    corpo = 'Corpo do E-mail'
-
-    # Criar um objeto yagmail
-    yag = yagmail.SMTP(remetente, senha)
-
-    #Enviar o e-mail
-    yag.send(to=destinatario, subject=assunto, contents=corpo)
-    
-    #Fechar a conexão
-    yag.close() """
-
-#Função para slavar dados de API
+#FunÃ§Ã£o para salvar dados de API
 def salvar_dados(dados, ficheiro):
     
     if isinstance(dados, pd.DataFrame):
@@ -39,38 +21,106 @@ def salvar_dados(dados, ficheiro):
     with open(ficheiro, "w") as file:
         file.write(dados_str)
 
-# Função para ir buscar os dados meteorológicos   
-def buscar_dados_clima(latitude_user, longitude_user):
+
+def city_to_coordinates(city_name):
+    geolocator = Nominatim(user_agent="my_geocoder")
+    location = geolocator.geocode(city_name)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        print(f"Coordinates for '{city_name}' not found.")
+        return None, None
+
+
+def display_info_clima(frame, title, data):
+        frame.config(bg="#4B0082")    
+        frame.pack(padx=10, pady=10, fill="both", expand=True)
+        tk.Label(frame, text=title, font=("Helvetica", 16), fg="white", bg="#4B0082").pack()  # Letra branca
+        tk.Label(frame, text=data, font=("Helvetica", 12), justify="left", fg="white", bg="#4B0082").pack()  # Letra branca
+
+# FunÃ§Ã£o para ir buscar os dados meteorolÃ³gicos   
+def buscar_dados_historicos(city_nameh, start_dateh, end_dateh):
     
-    # Configuração do cliente da API Open-Meteo com cache e tentativas de retransmissão em caso de erro
+    latitude_userh, longitude_userh = city_to_coordinates(city_nameh)
+    if latitude_userh is None or longitude_userh is None:
+        return  # Exit function if coordinates not found
+
+    cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+	"latitude": latitude_userh,
+	"longitude": longitude_userh,
+	"start_date": start_dateh,
+	"end_date": end_dateh,
+	"hourly": ["temperature_2m", "relative_humidity_2m"]
+}   
+    responses = openmeteo.weather_api(url, params=params)
+
+    global response
+    response = responses[0]
+   
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+	    start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	    end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = hourly.Interval()),
+	    inclusive = "left"
+    )}
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+
+    # CriaÃ§Ã£o da janela tkinter
+    window = tk.Tk()
+    window.title("Meteorologia")
+    window.configure(bg="#4B0082")
+    current_frame = tk.Frame(window)
+    display_info_clima(current_frame, "Informacao Atual", hourly_data)
+    salvar_dados(hourly_data, "Informacao_historica")
+        
+def buscar_dados_clima(city_name, start_hour, end_hour):
+    
+    latitude_user, longitude_user = city_to_coordinates(city_name)
+    if latitude_user is None or longitude_user is None:
+        return  # Exit function if coordinates not found
+
+    # ConfiguraÃ§Ã£o do cliente da API Open-Meteo com cache e tentativas de retransmissÃ£o em caso de erro
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
     openmeteo = openmeteo_requests.Client(session=retry_session)
     
-    # A ordem das variáveis em horário ou diário é importante para atribuí-las corretamente abaixo
+    start_time = datetime.datetime.now().replace(hour=start_hour, minute=0, second=0, microsecond=0).isoformat()
+    end_time = datetime.datetime.now().replace(hour=end_hour, minute=0, second=0, microsecond=0).isoformat()
+
+
+    # A ordem das variÃ¡veis em horÃ¡rio ou diÃ¡rio Ã© importante para atribuÃ­-las corretamente abaixo
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
         "latitude": latitude_user,
         "longitude":  longitude_user,
         "current": ["temperature_2m", "relative_humidity_2m", "rain", "wind_speed_10m"],
         "hourly": ["temperature_2m", "relative_humidity_2m", "rain", "wind_speed_10m"],
-        "daily": ["temperature_2m_max", "temperature_2m_min"]
+        "daily": ["temperature_2m_max", "temperature_2m_min"],
+        "start_hour": start_time,
+        "end_hour": end_time
     }
     responses = openmeteo.weather_api(url, params=params)
 
-    # Processamento da primeira localização.               Importante----Adicionar um loop for para múltiplas localizações
+    # Processamento da primeira localizaÃ§Ã£o.               Importante----Adicionar um loop for para mÃºltiplas localizaÃ§Ãµes
     global response
     response = responses[0]
     
    
-    # Função para exibir informações meteorológicas em uma janela
-    def display_info_clima(frame, title, data):
-            frame.config(bg="#4B0082")    
-            frame.pack(padx=10, pady=10, fill="both", expand=True)
-            tk.Label(frame, text=title, font=("Helvetica", 16), fg="white", bg="#4B0082").pack()  # Letra branca
-            tk.Label(frame, text=data, font=("Helvetica", 12), justify="left", fg="white", bg="#4B0082").pack()  # Letra branca
+    
 
-    # Exibição das informações meteorológicas atuais         
+    # ExibiÃ§Ã£o das informaÃ§Ãµes meteorolÃ³gicas atuais         
     def informacao_atual():
             current_data = (
                 f"Temperatura: {response.Current().Variables(0).Value()} C\n"
@@ -83,7 +133,7 @@ def buscar_dados_clima(latitude_user, longitude_user):
             salvar_dados(current_data, "informacao_atual")
             
                     
-    # Exibição das informações meteorológicas horárias 
+    # ExibiÃ§Ã£o das informaÃ§Ãµes meteorolÃ³gicas horÃ¡rias 
     def informacao_horaria():
         hourly_data = pd.DataFrame(data={
             "Temperatura": response.Hourly().Variables(0).ValuesAsNumpy(),
@@ -91,11 +141,12 @@ def buscar_dados_clima(latitude_user, longitude_user):
             "Chuva": response.Hourly().Variables(2).ValuesAsNumpy(),
             "Velocidade Vento": response.Hourly().Variables(3).ValuesAsNumpy()
         })
+       
         hourly_frame = tk.Frame(window)
         display_info_clima(hourly_frame, "Informacao Horaria", hourly_data)
         salvar_dados(hourly_data, "informacao_horaria")
 
-    # Exibição das informações meteorológicas diárias       
+    # ExibiÃ§Ã£o das informaÃ§Ãµes meteorolÃ³gicas diÃ¡rias       
     def informacao_diaria():
         daily_data = pd.DataFrame(data={
             "Max Temperatura": response.Daily().Variables(0).ValuesAsNumpy(),
@@ -107,7 +158,7 @@ def buscar_dados_clima(latitude_user, longitude_user):
         
 
 
-    # Criação da janela tkinter
+    # CriaÃ§Ã£o da janela tkinter
     window = tk.Tk()
     window.title("Meteorologia")
     window.configure(bg="#4B0082")
@@ -115,7 +166,7 @@ def buscar_dados_clima(latitude_user, longitude_user):
     frame_botoes = tk.Frame(window, bg="#4B0082")
     frame_botoes.pack(pady=20)
 
-    # Botões e posicionamento na janela
+    # BotÃµes e posicionamento na janela
     botao1 = tk.Button(frame_botoes, text="Informacao atual", command=informacao_atual)
     botao1.pack(side=tk.LEFT, padx=10)
 
@@ -128,33 +179,69 @@ def buscar_dados_clima(latitude_user, longitude_user):
     
     window.mainloop()
 
+
 def iniciar_interface():
     root = tk.Tk()
     root.title("METEOROLOGIA")
     root.configure(bg="#4B0082")
 
     def buscar_clima():
-        latitude = latitude_entry.get()
-        longitude = longitude_entry.get()
-        buscar_dados_clima(latitude, longitude)
-        root.destroy()  # Fecha a janela de entrada após buscar os dados
+        city_name = cidade_entry.get()
+        hora_inicial = int(hora_inicial_entry.get())
+        hora_final = int(hora_final_entry.get())
 
-    latitude_label = tk.Label(root, text="Latitude:", bg="#4B0082", fg="white")
-    latitude_label.pack(pady=10)
-    latitude_entry = tk.Entry(root)
-    latitude_entry.pack(pady=5)
+        buscar_dados_clima(city_name, hora_inicial, hora_final)
 
-    longitude_label = tk.Label(root, text="Longitude:", bg="#4B0082", fg="white")
-    longitude_label.pack(pady=10)
-    longitude_entry = tk.Entry(root)
-    longitude_entry.pack(pady=5)
+    def buscar_historicos():
+        city_nameh = cidadeh_entry.get()
+        start_dateh = start_dateh_entry.get()
+        end_dateh = end_dateh_entry.get()
 
-    buscar_button = tk.Button(root, text="Buscar Dados Meteorologicos", command=buscar_clima)
-    buscar_button.pack(pady=20)
+        buscar_dados_historicos(city_nameh, start_dateh, end_dateh)
+        
+    
+    # Widgets para entrada de dados
+    cidade_label = tk.Label(root, text="Cidade:", bg="#4B0082", fg="white")
+    cidade_label.pack(pady=10)
+    cidade_entry = tk.Entry(root)
+    cidade_entry.pack(pady=5)
 
-  
+    hora_inicial_label = tk.Label(root, text="Hora Inicial (0-23):", bg="#4B0082", fg="white")
+    hora_inicial_label.pack(pady=10)
+    hora_inicial_entry = tk.Entry(root)
+    hora_inicial_entry.pack(pady=5)
+
+    hora_final_label = tk.Label(root, text="Hora Final (0-23):", bg="#4B0082", fg="white")
+    hora_final_label.pack(pady=10)
+    hora_final_entry = tk.Entry(root)
+    hora_final_entry.pack(pady=5)
+
+    # BotÃµes para acionar as buscas
+    buscar_button_clima = tk.Button(root, text="Buscar Dados Meteorologicos", command=buscar_clima)
+    buscar_button_clima.pack(pady=10)
+
+    # Widgets para entrada de dados histÃ³ricos
+    cidadeh_label = tk.Label(root, text="Cidade historica:", bg="#4B0082", fg="white")
+    cidadeh_label.pack(pady=10)
+    cidadeh_entry = tk.Entry(root)
+    cidadeh_entry.pack(pady=5)
+
+    horah_inicial_label = tk.Label(root, text="Data Inicial historica (yy-mm-dd):", bg="#4B0082", fg="white")
+    horah_inicial_label.pack(pady=10)
+    start_dateh_entry = tk.Entry(root)
+    start_dateh_entry.pack(pady=5)
+
+    horah_final_label = tk.Label(root, text="Data Final historica (yy-mm-dd):", bg="#4B0082", fg="white")
+    horah_final_label.pack(pady=10)
+    end_dateh_entry = tk.Entry(root)
+    end_dateh_entry.pack(pady=5)
+
+    buscar_button_hist = tk.Button(root, text="Buscar Dados Meteorologicos Historicos", command=buscar_historicos)
+    buscar_button_hist.pack(pady=20)
+
 
     root.mainloop()
+
     
 # Iniciar a interface para inserir latitude e longitude
 iniciar_interface()
